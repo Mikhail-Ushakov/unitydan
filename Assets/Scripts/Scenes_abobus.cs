135f4e3390f1350eccadb2514e3e101b26c2ab2a
Mikhail-Ushakov <vivismvi@yandex.ru>
2023-09-30 16:46:22 +0300

    frst

---
+++ Assets/Scripts/Scenes_abobus.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.SceneManager;
+
+public class Scenes_abobus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void game_start()
+    {
+        UnityEngine.SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
